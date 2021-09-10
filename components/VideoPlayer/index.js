9856c99b17b271264d2a3b9bc06da9035f1cfaaf
Chris <chris.gargotta@gmail.com>
2021-09-10 09:43:02 -0500

    Fix css for video embeds

--- components/VideoPlayer/index.js
+++ components/VideoPlayer/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from '../../styles/Home.module.css';
 
 const VideoPlayer = ({ embedId }) => (
-  <div className="video-responsive">
+  <div className={styles.video}>
     <iframe
       width="853"
       height="480"
